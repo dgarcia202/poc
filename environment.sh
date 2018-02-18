ce0dfa47876143e42d035859ea55a10406496db4
dgarcia202 <wft2000@gmail.com>
2018-02-18 14:52:16 +0100

    enable ssh access and jkenkins. force yes on install.

--- environment.sh
+++ environment.sh
@@ -1,26 +1,30 @@
+#enable ssh access
+sudo apt-get -y install openssh-server
+sudo service ssh status
+
 #node and npm
 sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo apt-get install nodejs-legacy
+sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo apt-get -y install nodejs-legacy
 nodejs --version
 npm --version
 
 #git
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get -y install git
 git --version
 
 #java8
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get -y install oracle-java8-installer
 java -version
 
 #docker
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -35,5 +39,12 @@ sudo add-apt-repository \
    stable"
    
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y install docker-ce
 sudo docker run hello-world
+
+#jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
+
